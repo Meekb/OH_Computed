9e255c8b8c7208fe67662e323e1f828cecb2d45a
Beth <bethm.meeker@gmail.com>
2021-12-05 11:01:24 -0700

    Add some careers

--- pages/careers.vue
+++ pages/careers.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <h2>This is our Careers Page</h2>
+  <div class="careers-page">
+    <header class="careers-header">
+      <NuxtLink to="/" class="home-link">Back</NuxtLink>
+      <h2 class="careers-heading-text">Join The Overlook Team</h2>
+    </header>
+    <div class="career-list-container">
+      <ul>
+        <li>
+          Off-season Caretaker
+          <p class="job-details">
+            Now accepting applications for off-season Caretaker. Must have great regard for policies and procedures. Will be responsible for maintaining Boiler Room and plowing immediate grounds around entrances and exits when weather breaks at the end of the season. You will be encouraged to bring your family for the duration of the stay. All meals will be pre-stocked in our main kitchen for you and your family to last the winter. Must be strong of mind and prepared for the gravity of total isolation. Once the snow comes there will be no way to get out; a clean bill of health is required for all in the Caretaker party. Request an application from our Front Desk team! Please be ready to provide personal and professional references.
+          </p>
+        </li>
+        <li>
+          Pre-Closure Cleaning Crew
+          <p class="job-details">
+            Now accepting applications for our Pre-Closure Cleaning Crew. We clean and prep all rooms to be unoccupied for the winter season. We take pride in our pre-closure processes that ensure our rooms are fully ready for immediate re-opening when the snow clears. Must be able to stand for long periods of time and be able to lift up to 55lbs. Hiring 10 people to work 4 ten-hour shifts, paying $15/hr. Tentative start date is November 30th. Request an application from our Front Desk team!
+          </p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,5 +30,25 @@ export default {
 </script>
 
 <style scoped>
-
+.careers-page {
+  background-color: #FFFACD;
+  height: 100vh;
+}
+.careers-header {
+  padding-top: 10px;
+}
+.careers-heading-text {
+  text-align: center;
+}
+.home-link {
+  margin-left: 10px;
+  font-size: 18px;
+}
+.career-list-container {
+  font-size: 20px;
+}
+.job-details {
+  margin-left: 25px;
+  margin-right: 195px;
+}
 </style>
